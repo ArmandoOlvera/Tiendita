f89e5c92006f65f3132e92e5ce53ad7e44c37d66
ArmandoOlvera <1630066@upv.edu.mx>
2019-01-24 18:43:46 +0000

    first commit con categorais

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-24 17:52:42 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-24T17:52:42+00:00	INFO 200.95.170.85	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
+2019-01-24T17:57:56+00:00	INFO 200.95.170.85	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
